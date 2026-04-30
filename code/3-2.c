a2dd4473c817f676df6e30b35c19547e69fc0a5b
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-04-30 16:40:36 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     
     for(int i=argc; i>0; i--){
-        for(int j=1; j<i; j++){
+        for(int j=1; j<i-1; j++){
             if(strcmp(argv[j],argv[j+1]) == 1){
                 char *temp = argv[j];
                 argv[j] = argv[j+1];
