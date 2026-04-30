ea0fb524a41a1996bb23b085460a0d298a9a6028
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-04-30 17:22:37 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,7 +6,7 @@ int main() {
     char *names[] = {"apple", "mango", "ant", "banana", "cat", "anaconda"};
     int n = sizeof(names) / sizeof(names[0]);
 
-    for(int i=n; i>=0; i--){
+    for(int i=n-1; i>=0; i--){
         for(int j=0; j<i; j++){
             if(strcmp(names[j],names[j+1]) == 1){
                 char* temp = *(names + j);
